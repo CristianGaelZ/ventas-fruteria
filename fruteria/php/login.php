6db316e9a6538e5e0f027c51e7e09c27563f795a
CristianGaelZ <2330360@upv.edu.com>
2024-10-16 19:34:26 -0600

    ventas-fruteria

---
+++ fruteria/php/login.php
@@ -0,0 +1,11 @@
+<link rel='stylesheet' type='text/css' href='../css/nuevousuario.css'>
+
+<form action="login-conexion.php" method="POST">
+  <label for="email">Email:</label>
+  <input type="email" name="email" required>
+  
+  <label for="password">Contraseña:</label>
+  <input type="password" name="password" required>
+  
+  <button type="submit">Ingresar</button>
+</form>
